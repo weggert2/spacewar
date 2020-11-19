3ea79228f574b6894266eca17d241a28ba45e2f8
Bill Eggert <beggert@relativityspace.com>
2020-11-19 09:42:17 -0800

    Add explanation for why we choose inheritance instead of ECS

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -2,6 +2,27 @@
 
 #include <SFML/System/Vector2.hpp>
 
+/**
+ * I'm going for an inheritance based approach instead of an entity component
+ * system. But why?!? Entity component systems are always better!
+ *
+ * Well, our game is dead simple. The only moving parts are ships and
+ * bullets. The ships are basically identical. We'll have no problem
+ * maintaining a simple inheritance tree:
+ *
+ *           ------Entity------
+ *          /        |         \
+ *       Ship      Bullet   Black Hole
+ *      /    \
+ *   Enemy   Player
+ *
+ * If we were going to start adding new ship types, or have upgrades
+ * to the ships, different bullets, bosses, etc, etc, then an ECS would make
+ * sense. Therefore, this game doesn't scale well, but that's ok - it isn't
+ * meant to. We're trading rapid development time for scalability. In the
+ * interest of actually finishing, we'll go with the easy route.
+ */
+
 class Entity
 {
 public:
