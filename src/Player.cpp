c42305256c8089c9e72fc714aff0961a2c89746a
Bill Eggert <beggert@relativityspace.com>
2020-11-26 07:26:54 -0800

    Change Player to be a component

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,73 +1,73 @@
-#include "Player.hpp"
+// #include "Player.hpp"
 
-#include "math_utils.hpp"
+// #include "math_utils.hpp"
 
-#include <cmath>
-#include <iostream>
+// #include <cmath>
+// #include <iostream>
 
-Player::Player(
-    const sf::Texture &texture):
-        mBody(),
-        velocity(0.0f),
-        omega(0.0f)
-{
+// Player::Player(
+//     const sf::Texture &texture):
+//         mBody(),
+//         velocity(0.0f),
+//         omega(0.0f)
+// {
 
-    mBody.setTexture(texture);
-    mBody.setScale(0.4f, 0.4f);
+//     mBody.setTexture(texture);
+//     mBody.setScale(0.4f, 0.4f);
 
-    /* Approximates the center of mass as the blue "cockpit" */
-    const auto size  = mBody.getTexture()->getSize();
-    const auto scale = mBody.getScale();
-    const sf::Vector2f spriteSize(
-        size.x * scale.x,
-        size.y * scale.y);
+//     /* Approximates the center of mass as the blue "cockpit" */
+//     const auto size  = mBody.getTexture()->getSize();
+//     const auto scale = mBody.getScale();
+//     const sf::Vector2f spriteSize(
+//         size.x * scale.x,
+//         size.y * scale.y);
 
-    /* 1.35 = magic number found by fiddling. */
-    mBody.setOrigin(spriteSize.x, 1.35f*spriteSize.y);
-}
+//     /* 1.35 = magic number found by fiddling. */
+//     mBody.setOrigin(spriteSize.x, 1.35f*spriteSize.y);
+// }
 
-void Player::setImpulseUp(
-    const bool choice)
-{
-    velocity = choice ? -playerLinearSpeed : 0.0f;
-}
+// void Player::setImpulseUp(
+//     const bool choice)
+// {
+//     velocity = choice ? -playerLinearSpeed : 0.0f;
+// }
 
-void Player::setImpulseDown(
-    const bool choice)
-{
-    velocity = choice ? playerLinearSpeed : 0.0f;
-}
+// void Player::setImpulseDown(
+//     const bool choice)
+// {
+//     velocity = choice ? playerLinearSpeed : 0.0f;
+// }
 
-void Player::setRotateLeft(
-    const bool choice)
-{
-    omega = choice ? -playerAngularSpeed : 0.0f;
-}
+// void Player::setRotateLeft(
+//     const bool choice)
+// {
+//     omega = choice ? -playerAngularSpeed : 0.0f;
+// }
 
-void Player::setRotateRight(
-    const bool choice)
-{
-    omega = choice ? playerAngularSpeed : 0.0f;
-}
+// void Player::setRotateRight(
+//     const bool choice)
+// {
+//     omega = choice ? playerAngularSpeed : 0.0f;
+// }
 
-void Player::update(
-    const sf::Time &deltaTime)
-{
-    /* For now, we'll do constant linear velocity.
-     * TODO: Add momentum, which requires a "force" balance and then
-     * a velocity update. */
+// void Player::update(
+//     const sf::Time &deltaTime)
+// {
+//     /* For now, we'll do constant linear velocity.
+//      * TODO: Add momentum, which requires a "force" balance and then
+//      * a velocity update. */
 
-    const float dt = deltaTime.asSeconds();
-    const float dTheta = omega*dt;
-    mBody.rotate(dTheta);
+//     const float dt = deltaTime.asSeconds();
+//     const float dTheta = omega*dt;
+//     mBody.rotate(dTheta);
 
-    /* Rotate the unit y vector by theta to get the heading direction. */
-    const float theta = toRad(mBody.getRotation());
-    const sf::Vector2f n(-std::sin(theta), std::cos(theta));
-    mBody.move(velocity*n*dt);
-}
+//     /* Rotate the unit y vector by theta to get the heading direction. */
+//     const float theta = toRad(mBody.getRotation());
+//     const sf::Vector2f n(-std::sin(theta), std::cos(theta));
+//     mBody.move(velocity*n*dt);
+// }
 
-sf::Sprite &Player::get()
-{
-    return mBody;
-}
+// sf::Sprite &Player::get()
+// {
+//     return mBody;
+// }
--- src/Player.hpp
+++ src/Player.hpp
@@ -1,31 +1,37 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
-
-class Player
+class Player : public entityx::Component<SpaceShip>
 {
 public:
-    explicit Player(
-        const sf::Texture &texture);
-
-    sf::Sprite &get();
-
-    void setImpulseUp(const bool choice);
-    void setImpulseDown(const bool choice);
-    void setRotateLeft(const bool choice);
-    void setRotateRight(const bool choice);
-
-    void update(
-        const sf::Time &deltaTime);
-
-private:
-    sf::Sprite mBody;
-
-    /** The linear ang angular velocities. */
-    float velocity;
-    float omega;
-
-    static constexpr float playerLinearSpeed = 200.0;
-    static constexpr float playerAngularSpeed = 100.0;
-    static const std::string texturePath;
+    Player() = default;
 };
+
+// #include <SFML/Graphics.hpp>
+
+// class Player
+// {
+// public:
+//     explicit Player(
+//         const sf::Texture &texture);
+
+//     sf::Sprite &get();
+
+//     void setImpulseUp(const bool choice);
+//     void setImpulseDown(const bool choice);
+//     void setRotateLeft(const bool choice);
+//     void setRotateRight(const bool choice);
+
+//     void update(
+//         const sf::Time &deltaTime);
+
+// private:
+//     sf::Sprite mBody;
+
+//     /** The linear ang angular velocities. */
+//     float velocity;
+//     float omega;
+
+//     static constexpr float playerLinearSpeed = 200.0;
+//     static constexpr float playerAngularSpeed = 100.0;
+//     static const std::string texturePath;
+// };
