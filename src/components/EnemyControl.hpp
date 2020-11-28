5160e90c042993fa112d2d50c009762801454f47
Bill Eggert <beggert@relativityspace.com>
2020-11-28 13:25:36 -0800

    Add EnemyControl component

---
+++ src/components/EnemyControl.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <entityx/Entity.h>
+
+class EnemyControl : public entityx::Component<EnemyControl>
+{
+public:
+    EnemyControl() = default;
+};
--- src/components/Player.hpp
+++ src/components/Player.hpp
@@ -7,33 +7,3 @@ class Player : public entityx::Component<Player>
 public:
     Player() = default;
 };
-
-// #include <SFML/Graphics.hpp>
-
-// class Player
-// {
-// public:
-//     explicit Player(
-//         const sf::Texture &texture);
-
-//     sf::Sprite &get();
-
-//     void setImpulseUp(const bool choice);
-//     void setImpulseDown(const bool choice);
-//     void setRotateLeft(const bool choice);
-//     void setRotateRight(const bool choice);
-
-//     void update(
-//         const sf::Time &deltaTime);
-
-// private:
-//     sf::Sprite mBody;
-
-//     /** The linear ang angular velocities. */
-//     float velocity;
-//     float omega;
-
-//     static constexpr float playerLinearSpeed = 200.0;
-//     static constexpr float playerAngularSpeed = 100.0;
-//     static const std::string texturePath;
-// };
