e3cd83e744ba6774d99c60b0fdeedc7ae048c19c
Bill Eggert <william.eggert@spacex.com>
2020-11-29 21:49:08 -0800

    Tweak to KeyManager

--- src/core/KeyManager.cpp
+++ src/core/KeyManager.cpp
@@ -17,15 +17,11 @@ void KeyManager::updateKey(
 bool KeyManager::isPressed(
     const sf::Keyboard::Key key) const
 {
-    /* If key is not in the map already, C++ makes an entry for us.
-     * I don't like to rely on that behavior though, so we'll add
-     * in a bit of extra logic. */
-
     const auto found = pressedMap.find(key);
     if (found == pressedMap.end())
     {
         return false;
     }
 
-    return pressedMap.at(key);
+    return found->second;
 }
